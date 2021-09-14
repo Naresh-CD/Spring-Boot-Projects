c65415e2db81ca646164cf7c4f4769b6a5f26e90
Naresh <nareshdubbaka001@gmail.com>
2021-09-13 23:37:23 -0500

    This is Curious Geek Blog Project

---
+++ CuriousGeekBlog/src/test/java/com/curiousgeek/curiousgeekblog/CuriousGeekBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curiousgeek.curiousgeekblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuriousGeekBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
