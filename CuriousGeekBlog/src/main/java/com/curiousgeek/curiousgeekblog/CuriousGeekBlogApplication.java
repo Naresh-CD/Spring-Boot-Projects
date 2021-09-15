4bf741b18f3dbe16a40df9a6b3f2fe6cb85741e8
Naresh <nareshdubbaka001@gmail.com>
2021-09-14 23:48:22 -0500

    This is curious geek blog project

---
+++ CuriousGeekBlog/src/main/java/com/curiousgeek/curiousgeekblog/CuriousGeekBlogApplication.java
@@ -0,0 +1,13 @@
+package com.curiousgeek.curiousgeekblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuriousGeekBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CuriousGeekBlogApplication.class, args);
+    }
+
+}
