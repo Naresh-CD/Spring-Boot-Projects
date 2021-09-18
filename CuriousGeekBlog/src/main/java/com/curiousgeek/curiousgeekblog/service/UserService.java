76814d626b74efc1810946ebe0b3622b577ccd33
Naresh <nareshdubbaka001@gmail.com>
2021-09-18 00:29:49 -0500

    added extension class UserDetailsService

--- CuriousGeekBlog/src/main/java/com/curiousgeek/curiousgeekblog/service/UserService.java
+++ CuriousGeekBlog/src/main/java/com/curiousgeek/curiousgeekblog/service/UserService.java
@@ -2,8 +2,8 @@ package com.curiousgeek.curiousgeekblog.service;
 
 import com.curiousgeek.curiousgeekblog.model.User;
 import com.curiousgeek.curiousgeekblog.web.dto.UserRegistrationDto;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
-public interface UserService {
+public interface UserService extends UserDetailsService {
     User save(UserRegistrationDto registrationDto);
-
 }
